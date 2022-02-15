8e900edc96afeb3607e24b839deccf6c1ce04f27
Drashti Patel <drashti.0707@gmail.com>
2022-02-15 05:49:23 +0000

    Finish crispy forms

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,7 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now=True)
 
 # Create your models here.
